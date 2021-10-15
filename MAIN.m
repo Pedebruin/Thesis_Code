286a75d1b8cdc8828f2e1d6692fabfb49b4a6097
Pim de Bruin <pedebruin@hotmail.com>
2021-10-15 14:10:59 +0200

    Some plot changes

--- MAIN.m
+++ MAIN.m
@@ -5,9 +5,9 @@ set(0,'defaultTextInterpreter','latex','defaultAxesFontSize',12);
 %% Parameters & Settings
 % Parameters
 L = 300;    % mm
-h = 1;     % mm
+h = 2;     % mm
 
-N = 30;%L/h;      % Number of Horizontal Nodes (minimum 2)
+N = 10;%L/h;      % Number of Vertical Nodes (minimum 2)
 
 E = 210E9;
 mu = 0.3;
@@ -16,18 +16,19 @@ alpha = 10;
 beta = 2;
 zeta = 0.1;
 
-simulationSettings.damping = true;
-    simulationSettings.dampingModel = 'modal';
 simulationSettings.simulate = true;
-    simulationSettings.T = 100;       % s
+    simulationSettings.damping = true;
+    simulationSettings.dampingModel = 'modal';
+    simulationSettings.T = 25;       % s
     simulationSettings.dt = 0.1;   % s 
+    simulationSettings.plot = true;
 
 % Settings
 plotSettings.realMesh = false;
-plotSettings.modalAnalysis = true;
+plotSettings.modalAnalysis = false;
     plotSettings.modeNr = 3;
     plotSettings.A = 1e4;
-plotSettings.nodes = false;
+plotSettings.nodes = true;
 plotSettings.nodeNumbers = false;
 plotSettings.links = true;
 plotSettings.linkNumbers = false;
@@ -46,7 +47,7 @@ R = FEM.R;
 wmin = -0.1;
 wmax = 100;
 
-% Modal analysis simulation! P395
+% Modal decomposition! P395
 modal = solve(beam,'FrequencyRange',[wmin,wmax]);   % Solve
 Nmodes = length(modal.NaturalFrequencies);
 Phi = [modal.ModeShapes.ux;                         % Phi
@@ -64,10 +65,11 @@ Rext = zeros(numNodes*2,1);                                 % External Input
 Rphi = Phi'*Rext;                                         
 
 
-% Time simulation
+%% Time simulation
 if simulationSettings.simulate ==  true
     z0 = zeros(Nmodes,1);
-    z0(2) = 1e4;
+    z0(2) = 4e4;
+    z0(1) = 2e4;
     tvec = 0:simulationSettings.dt:simulationSettings.T;
 
     % y -> [z;zdot];!!!!!
@@ -79,38 +81,38 @@ if simulationSettings.simulate ==  true
     [t,y] = ode45(@(t,z) sysFun(t,z,A,B),tvec,y0);
     z = y(:,1:Nmodes)';
     
-    figure()
-    hold on
-    axis equal
-    grid on
-    xlabel 'mm'
-    ylabel 'mm'
-    xlim([0,L])
-    ylim([-L/2,L/2])
-    title 'Beam plot'
-    ax = gca;
-    simPlots = [];
-    for i = 1:length(t)
-        d = Phi*z(:,i);
-        
-        % Update Beam
-        updateBeam(links,nodes,d);
+    if simulationSettings.plot == true
+        figure()
+        hold on
+        axis equal
+        grid on
+        xlabel 'mm'
+        ylabel 'mm'
+        xlim([0,1.2*L])
+        ylim([-L/6,L/6])
+        title 'Beam plot'
+        ax = gca;
+        simPlots = [];
+        for i = 1:length(t)
+            d = Phi*z(:,i);
 
-        if ~isempty(simPlots)
-            delete(simPlots)
-        end
-        
-        % Plot beam
-        simPlots = plotBeam(nodes,links,plotSettings,ax); 
-        pause(0.01);
-    end 
+            % Update Beam
+            updateBeam(links,nodes,d);
+
+            if ~isempty(simPlots)
+                delete(simPlots)
+            end
+
+            % Plot beam
+            simPlots = plotBeam(nodes,links,plotSettings,ax); 
+            P = text(ax, L/3*2,L/6*2,['Time; ',num2str(t(i)),'/',num2str(t(end)),' s']);
+            simPlots = [simPlots, P];
+            pause(0.01);
+        end 
+    end
 end
 
-
-
-
-
-
+%% Single mode plot
 if plotSettings.modalAnalysis == true
     nr = plotSettings.modeNr;
     A = plotSettings.A;
@@ -130,15 +132,12 @@ if plotSettings.modalAnalysis == true
     plotBeam(nodes,links,plotSettings,ax);
 end
 
+%% Functions
 % odefun for ode45 etc..
 function yd = sysFun(t,y,A,B)
 % Kept the odefun as basic as possible, for future simulations!
 yd = A*y+B;
 end
-
-
-
-
 
 function [links,nodes] =  updateBeam(links,nodes,q)
     numNodes = length(nodes);
